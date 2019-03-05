0bb4d368d95bf7ce893718df66d8630ae617c1df
Bharat Kumar <bharatkumar1997@gmail.com>
2019-03-04 21:54:32 -0500

    Update curve_modeler.py
    Added the solution method for the linear equation using Gaussian elimination.

--- curve_modeler.py
+++ curve_modeler.py
@@ -25,5 +25,17 @@ def get_curve(data_points, degree):
         for l in range(0, i):
             sum += pow(data_points[l][0], range_n - k)
         coefficients.append(sum)
+        
+#[x[n], x[n-1], ... , x[0]]
+def get_solution_vector(matrix, values):
+    n = len(matrix)
+    new_matrix = matrix
 
-    
+    #gaussian elimination
+    for i in range(0, n):
+        for j in range(0, n):
+            if i != j and new_matrix[i][i] != 0:
+                ratio = (-1.0 * new_matrix[j][i])/new_matrix[i][i]
+                for k in range(0, n):
+                    value =  (ratio * new_matrix[i][k]) + new_matrix[j][k]
+                    new_matrix[j][k] = value

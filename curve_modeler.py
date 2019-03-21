3d80cb267d544fb6a6ff62956c93d1e62f60d695
Bharat Kumar <bharatkumar1997@gmail.com>
2019-03-20 22:22:22 -0400

    Update curve_modeler.py

--- curve_modeler.py
+++ curve_modeler.py
@@ -37,33 +37,66 @@ def get_curve(data_points, degree):
 
     return get_solution_vector(matrix, values)
 
+def update_row(matrix, row_index, ratio, pivot_row_index, n):
+    row = []
+    for i in range(0, n):
+        v = (ratio * matrix[pivot_row_index][i]) + matrix[row_index][i]
+        row.append(v)
+
+    return row
+
+def update_all_rows(matrix, pivot_row_index, pivot_position, n):
+    new_matrix = matrix
+    for i in range(0, n):
+        if i != pivot_row_index:
+            ratio = get_ratio(new_matrix, i, pivot_row_index, pivot_position, n)
+            row = update_row(new_matrix, i, ratio, pivot_row_index, n)
+            new_matrix[i] = row
+
+    return new_matrix
+
+def update_all_values(values, matrix, pivot_row_index, pivot_position, n):
+    new_values = values
+    for i in range(0, n):
+        if i != pivot_row_index:
+            ratio = get_ratio(matrix, i, pivot_row_index, pivot_position, n)
+            value = (ratio * values[pivot_row_index]) + values[i]
+            new_values[i] = value
+
+    return new_values
+
+def get_pivot_position(matrix, pivot_row_index, n):
+    pivot_value = matrix[pivot_row_index][0]
+    index = 0
+    while pivot_value == 0 and index < n:
+        index = index + 1
+        pivot_value = matrix[pivot_row_index][index]
+
+    return index
+
+def get_ratio(matrix, row_index, pivot_row_index, pivot_row_position, n):
+    ratio = (-1 * matrix[row_index][pivot_row_position]) / matrix[pivot_row_index][pivot_row_position]
+    return ratio
+
 #[x[0], x[1], ... , x[n]]
 def get_solution_vector(matrix, values):
     n = len(matrix)
 
     new_matrix = matrix
     new_values = values
-    #gaussian elimination
-
-    pivot = 0;
 
     for i in range(0, n):
-        for j in range(0, n):
-            if i != j and new_matrix[i][i] != 0:
-                ratio = (-1.0 * new_matrix[j][i])/new_matrix[i][i]
-                for k in range(0, n):
-                    value =  (ratio * new_matrix[i][k]) + new_matrix[j][k]
-                    new_matrix[j][k] = value
+        print("MATRIX")
+        print(new_matrix)
+        print("VALUES")
+        print(new_values)
+        pivot_position = get_pivot_position(new_matrix, i, n)
+        string = "Pivot Position = " + str(pivot_position)
+        print(string)
+        new_values = update_all_values(new_values, new_matrix, i, pivot_position, n)
+        new_matrix = update_all_rows(new_matrix, i, pivot_position, n)
 
-                value = (ratio * new_values[i]) + new_values[j]
-                new_values[j] = value
-
-    coefficients = [0 for i in range(0, n)]
-
-    for i in range(0, n):
-        coefficients[i] = new_values[i] / new_matrix[i][i]
-
-    return coefficients
+    return new_matrix
 
 def f(x, model):
     n = len(model)
@@ -103,16 +136,12 @@ def get_curves(points):
         e = e / estimate
         error[i] = (i, e)
 
-    sorted_error = error
-    sorted_error.sort(key = sort_key)
-
-    p = int((n + 1) / 2)
-    median = error[p]
-
     split = [False for i in range(0, n)]
 
-    for i in range(0, p):
-        split[sorted_error[0]] = True
+    tolerance = 0.2
+    for i in range(0, n):
+        if error[i][1] > tolerance:
+            split[error[i][0]] = True
 
     split_intervals = []
     start_index = 0;
@@ -144,3 +173,12 @@ def get_curves(points):
         degree = n
         curve = get_curve(list_points, degree)
         functions[i]  = curve
+
+def test():
+    matrix = [[1, 2, 3, 4],[2, 3, 1, 5], [3, 1, 2, 2], [3, 5, 2, 1]]
+    values = [1, 2, 3, 4]
+    solution = get_solution_vector(matrix, values)
+
+    print(solution)
+
+test()

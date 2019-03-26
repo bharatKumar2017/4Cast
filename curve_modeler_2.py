4eaa262b4e757834ed0ea751a2e1a3ea8a322c37
Bharat Kumar <bharatkumar1997@gmail.com>
2019-03-25 22:17:53 -0400

    Update curve_modeler_2.py

--- curve_modeler_2.py
+++ curve_modeler_2.py
@@ -1,5 +1,6 @@
 import csv
 import numpy
+import math
 
 def get_curve(data_points, degree):
     x_data = []
@@ -22,7 +23,25 @@ def get_average_error(model, data_points):
 
     avg = (1.0 * error) / n
 
+    value = math.sqrt(avg)
+
     return avg
+
+def get_error(x, model, data_points):
+    n = len(data_points)
+    y = 0
+    for i in range(0, n):
+        if x == data_points[i][0]:
+            y = data_points[i][1]
+
+    y_expected = f(x, model)
+
+    error = (y_expected - y) / (1.0 * y)
+
+    if error < 0:
+        error = -1 * error
+
+    return error
 
 def boolean_function(error, tolerance):
     if error > tolerance:
@@ -52,6 +71,37 @@ def get_curves(data_points, tolerance):
 
     return model
 
+def get_max_error(model, data_points):
+    n = len(data_points)
+    max_error = 0
+    for i in range(0, n):
+        e_percentage = (data_points[i][1] - f(data_points[i][0], model)) / (1.0 * data_points[i][1])
+        error = e_percentage
+        if error < 0:
+            error = -1 * error
+
+        if max_error < error:
+            max_error = error
+
+    return max_error
+
+def get_curves_max(data_points, tolerance):
+    upper_bound = len(data_points) - 1
+    lower_bound = 0
+    while upper_bound > lower_bound + 1:
+        mid = (upper_bound + lower_bound) / 2
+        degree = mid
+        model = get_curve(data_points, degree)
+        error = get_max_error(model, data_points)
+
+        c = boolean_function(error, tolerance)
+
+        if c == 0:
+            return model
+        elif c > 0:
+            lower_bound = mid
+        elif c < 0:
+            upper_bound = mid
 
 def f(x, model):
     n = len(model)
@@ -95,28 +145,129 @@ def convert_model_to_string(model):
                     model_string += " + " + str(model[i])
     return model_string
 
+def test_model(model, data_points):
+    model_points = []
+    n = len(data_points)
+    for i in range(0, n):
+        x = data_points[i][0]
+        y = f(x, model)
+        tuple = (x, y)
+        model_points.append(tuple)
+
+    return model_points
+
+def split_function(data_points, tolerance):
+    n = len(data_points)
+    model = get_curves(data_points, tolerance)
+
+    split = [False for i in range(0, n)]
+
+    for i in range(0, n):
+        if get_error(data_points[i][0], model, data_points):
+            split[i] = True
+
+    points = []
+    models = []
+
+    points.append(data_points[0])
+
+    intervals = []
+
+    for i in range(1, n):
+        if split[i] == split[i - 1]:
+            points.append(data_points[i])
+        else:
+            points.append(data_points[i])
+            model = get_curves_max(points, 0.005)
+            tuple = (points[0][0], data_points[i][0])
+            models.append(model)
+            intervals.append(tuple)
+
+    v = len(models)
+
+    if v == 0:
+        model = get_curves_max(points, 0.005)
+        tuple = (points[0][0], data_points[n - 1][0])
+        models.append(model)
+        intervals.append(tuple)
+
+    print(intervals)
+    models_final = []
+    intervals_final = []
+    v = len(intervals)
+
+    print(v)
+    for i in range(0, v):
+        if models[i] != None:
+            models_final.append(models[i])
+            intervals_final.append(intervals[i])
+
+    print(models_final)
+    models_and_intervals = (models_final, intervals_final)
+
+    print(models_and_intervals)
+    return models_and_intervals
+
+def f_split(x, function):
+    models = function[0]
+    intervals = function[1]
+
+    n = len(intervals)
+
+    index = 0
+
+    for i in range(0, n):
+        if x >= intervals[i][0] and x <= intervals[i][1]:
+             index = i
+             break
+
+    model = models[index]
+
+    value = f(x, model)
+
+    return value
+
+def test_model_split(model, data_points):
+    model_points = []
+    n = len(data_points)
+    for i in range(0, n):
+        x = data_points[i][0]
+        y = f_split(x, model)
+        tuple = (x, y)
+        model_points.append(tuple)
+
+    return model_points
+
 def test():
     data_points = [(1, 1.25), (2, 0.94), (3, 0.65), (4, 0.62), (5, 0.87), (6, 0.94), (7, 1.14), (8, 1.72), (9, 1.83), (10, 2.12), (11, 1.91), (12, 1.80), (13, 2.25), (14, 2.08), (15, 1.73)]
-    #
 
     print("DATA POINTS")
     print(data_points)
     print("\n")
 
-    model = get_curves(data_points, 0.005)
+    model = split_function(data_points, 0.005)
 
-    model_string = convert_model_to_string(model)
+    print("Average Error = 0.5%\n")
+
+    #model_string = convert_model_to_string(model)
 
     print("MODEL (equation form)")
-    print(model_string)
+    #print(model_string)
 
     print("\n")
     print("EXAMPLES")
 
-    print("x = ?")
-    x = int(input())
+    test_data = test_model_split(model, data_points)
 
-    y = f(x, model)
-    print((x, y))
+    print(test_data)
+
+    while True:
+        print("x = ?")
+        string = input()
+        if string == "quit":
+            return 0
+        x = int(string)
+        y = f_split(x, model)
+        print((x, y))
 
 test()
